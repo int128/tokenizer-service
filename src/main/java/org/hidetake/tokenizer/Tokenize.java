1b839e7bf1d62d6f4b45a5b0e66c248cde8d4d2a
int128 <int128@gmail.com>
2012-05-10 00:42:19 +0900

    add support for cross-domain requests

--- src/main/java/org/hidetake/tokenizer/Tokenize.java
+++ src/main/java/org/hidetake/tokenizer/Tokenize.java
@@ -3,6 +3,7 @@ package org.hidetake.tokenizer;
 import java.util.List;
 
 import javax.ws.rs.FormParam;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -20,7 +21,22 @@ public class Tokenize {
 	@Produces("application/json")
 	public Response post(@FormParam("text") String text) {
 		List<Token> tokens = this.tokenizer.tokenize(text);
-		return Response.ok().entity(tokens).build();
+		return Response.ok().entity(tokens)
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "POST,OPTIONS")
+				.build();
+	}
+
+	/**
+	 * Response for XHR pre-flight request.
+	 * @return
+	 */
+	@OPTIONS
+	public Response options() {
+		return Response.ok()
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "POST,OPTIONS")
+				.build();
 	}
 
 }
